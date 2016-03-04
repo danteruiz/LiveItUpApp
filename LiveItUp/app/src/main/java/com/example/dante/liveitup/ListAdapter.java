66fe6a70dd73541744239150d5001c80b076d0e9
Dante Ruiz <Dante@Dantes-MBP.hsd1.ca.comcast.net>
2016-03-03 20:52:58 -0800

    added clickable list

---
+++ LiveItUp/app/src/main/java/com/example/dante/liveitup/ListAdapter.java
@@ -0,0 +1,73 @@
+package com.example.dante.liveitup;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dante on 3/2/16.
+ */
+
+public class ListAdapter extends ArrayAdapter<ListElement>
+{
+    int resource;
+    Context context;
+    public List<ListElement> List = new ArrayList<ListElement>();
+
+
+    @Override
+    public void add(ListElement object) {
+        List.add(object);
+        super.add(object);
+    }
+
+    public void Clear()
+    {
+        List.clear();
+    }
+    public ListAdapter(Context _context, int _resource, List<ListElement> items) {
+        super(_context, _resource, items);
+        resource = _resource;
+        context = _context;
+    }
+
+
+
+
+        @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LinearLayout newView;
+
+        ListElement w = getItem(position);
+
+        // Inflate a new view if necessary.
+        if (convertView == null) {
+            newView = new LinearLayout(getContext());
+            String inflater = Context.LAYOUT_INFLATER_SERVICE;
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+            vi.inflate(resource,  newView, true);
+        } else {
+            newView = (LinearLayout) convertView;
+        }
+
+        // Fills in the view.
+        TextView tv = (TextView) newView.findViewById(R.id.itemText);
+             tv.setText("hello");
+
+        // Set a listener for the whole list item.
+        newView.setTag("hello");
+
+        return newView;
+    }
+}
+
--- LiveItUp/app/src/main/java/com/example/dante/liveitup/MainActivity.java
+++ LiveItUp/app/src/main/java/com/example/dante/liveitup/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private int progressStatus = 100;
     private TextView textView;
     private Handler handler = new Handler();
+    private ArrayList<ListElement> aList;
+    private ListAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        aList = new ArrayList<ListElement>();
+        aa = new ListAdapter(this, R.layout.list_view, aList);
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(aa);
+        myListView.setOnItemClickListener(new ListClickHandler());
+        // Read from appInfo the list of friends, and sets it.
+        for (int i = 0; i < 6; i++) {
+            ListElement le = new ListElement();
+            le.textLabel = "hello";
+            le.buttonLabel = "Choose";
+            aList.add(le);
+        }
+        aa.notifyDataSetChanged();
+
     }
 
-    public void buttonClick(View v){
+    public void Stats(View v)
+    {
+        aa.clear();
         progressStatus += 5;
         progressBar.setProgress(progressStatus);
     }
-    public void buttonClick2(View v){
+    public void Inv(View v)
+    {
+        aa.clear();
         progressStatus -= 5;
         progressBar.setProgress(progressStatus);
+    }
+
+    public void Actions(View v)
+    {
+        aa.clear();
     }
 }
